7d2a2f10813a311260182f10be01b82c0b444c1b
light419 <refesseteddy@gmail.com>
2023-09-07 11:17:43 +0200

    exercice js avec des calculs

---
+++ echauffement-general/exo2.js
@@ -0,0 +1,11 @@
+let resultataddition = 3 + 4;
+console.log("resultat addition : " + resultataddition);
+
+let resultatsoustraction = 10 - 3;
+console.log("resultat soustraction : " + resultatsoustraction);
+
+let resultatmultiplication = 7 * 7;
+console.log("resultat multiplication : " + resultatmultiplication);
+
+let resultatdivision = 21 / 3;
+console.log("resultat division : " + resultatdivision);
